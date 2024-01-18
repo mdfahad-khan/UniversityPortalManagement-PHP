8e2424ee8aec28e3e0588762c9a29ca040f9be07
fahad <evanahmedfahad@gmail.com>
2024-01-18 20:06:15 +0600

    university portal management system project added build using php css javascript

---
+++ views/accounts/account_payment.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style="background-color:#87CEFA;">
+        <table align="center" border="1px">
+            <tr>
+                <td>
+                    <?php
+                        require 'account_home_logout.php';
+                    ?>
+                </td>
+                <tr>
+                    <td>
+                        <?php
+                        require 'accounts_menu.php';
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button>Show Request</button>
+                        
+                    </td>
+                </tr>
+            </tr>
+       
+        </table>
+       
+
+
+    </div>
+    
+</body>
+</html>
---
+++ views/select_rule.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h1>SELECT YOUR RULE </h1>
+        <button><a href="signup/Signup.php">STUDENT</a> </button>
+        <button><a href="signup/faculty_signup.php">FACULTY</a> </button>
+        <button><a href="singup.php">ACCOUNT</a> </button>
+        <button><a href="singup.php">ADMIN</a> </button>
+
+    </div>
+
+</body>
+
+</html>
